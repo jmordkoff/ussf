c5baaecb7b54a38d260a22f8a4a6b237f8023d68
Jeremy Mordkoff <jeremy.mordkoff@gmail.com>
2018-01-27 00:13:22 -0500

    django 2.0

--- ussfreg/admin.py
+++ ussfreg/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import WebhookMessage, Player, Competition
+
+
+
+admin.site.register(WebhookMessage)
+admin.site.register(Player)
+admin.site.register(Competition)
